87ac473cf7a61492385caf09513294ed822b1ced
fourbarman <maks.java@yandex.ru>
2024-03-03 12:29:13 +0400

    Init project

---
+++ src/test/java/com/fourbarman/burgercloud/BurgerCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourbarman.burgercloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
